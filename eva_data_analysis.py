f4f44fdd10445c4f7ede1558537d670d4779ddde
Colin Sauze <colin.sauze@noc.ac.uk>
2024-07-02 12:29:46 +0100

    Add commandline functionality to the script

--- eva_data_analysis.py
+++ eva_data_analysis.py
@@ -1,26 +1,35 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-
-# https://data.nasa.gov/resource/eva.json
-# Input and output files
-data_f = './eva-data.json'
-data_t = './eva-data.csv'
-g_file = './cumulative_eva_graph.png'
-
-data = pd.read_json(data_f, convert_dates=['date'])
-data['eva'] = data['eva'].astype(float)
-data.dropna(axis=0, inplace=True)
-data.sort_values('date', inplace=True)
-
-data.to_csv(data_t, index=False)
-
-data['duration_hours'] = data['duration'].str.split(":").apply(lambda x: int(x[0]) + int(x[1])/60)
-data['cumulative_time'] = data['duration_hours'].cumsum()
+import sys
 
 
-plt.plot(data.date,data.cumulative_time, 'ko-')
-plt.xlabel('Year')
-plt.ylabel('Total time spent in space to date (hours)')
-plt.tight_layout()
-plt.savefig(g_file)
-plt.show()
+if __name__ == '__main__':
+
+    if len(sys.argv) < 3:
+        data_f = './eva-data.json'
+        data_t = './eva-data.csv'
+        print(f'Using default input and output filenames')
+    else:
+        data_f = sys.argv[1]
+        data_t = sys.argv[2]
+        print(f'Using custom input and output filenames')
+
+    g_file = './cumulative_eva_graph.png'
+
+    data = pd.read_json(data_f, convert_dates=['date'])
+    data['eva'] = data['eva'].astype(float)
+    data.dropna(axis=0, inplace=True)
+    data.sort_values('date', inplace=True)
+
+    data.to_csv(data_t, index=False)
+
+    data['duration_hours'] = data['duration'].str.split(":").apply(lambda x: int(x[0]) + int(x[1])/60)
+    data['cumulative_time'] = data['duration_hours'].cumsum()
+
+
+    plt.plot(data.date,data.cumulative_time, 'ko-')
+    plt.xlabel('Year')
+    plt.ylabel('Total time spent in space to date (hours)')
+    plt.tight_layout()
+    plt.savefig(g_file)
+    plt.show()
