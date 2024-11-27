dfed1979bb7940ca8f7ddab80a8c93c403cd6723
Colin Sauze <colin.sauze@noc.ac.uk>
2024-11-27 11:33:34 +0000

    Added some comments and removed unused variable

--- eva_data_analysis.py
+++ eva_data_analysis.py
@@ -7,21 +7,26 @@ input_file = open('eva-data.json', 'r')
 output_file = open('eva-data.csv','w')
 graph_file = 'myplot.png'
 
-fieldnames = ("EVA #", "Country", "Crew    ", "Vehicle", "Date", "Duration", "Purpose")
 
+# Read the data from a JSON file into a Pandas dataframe
 eva_df = pd.read_json(input_file, convert_dates=['date'])
 eva_df['eva'] = eva_df['eva'].astype(float)
+
+# Clean the data by removing any incomplete rows and sort by date
 eva_df.dropna(axis=0, inplace=True)
 eva_df.sort_values('date',inplace=True)
 
+# Save dataframe to CSV file for later analysis
 eva_df.to_csv(output_file, index=False)
 
+# Calculate cumulative time spent in space over years
 eva_df['duration_hours'] =  eva_df['duration'].str.split(":").apply(lambda x: int(x[0]) + int(x[1])/60)
 eva_df['cumulative_time'] = eva_df['duration_hours'].cumsum()
 
+# Plot the cumulative time
 plt.plot(eva_df['date'],eva_df['cumulative_time'], 'ko-')
 plt.xlabel('Year')
 plt.ylabel('Total time spent in space to date (hours)')
 plt.tight_layout()
 plt.savefig(graph_file)
-plt.show()
+plt.show() 
